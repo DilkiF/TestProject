ac715e5b10f560c7a48d52d90d9f2b660f0dcae6
DilkiF <mdilkinfernando@gmail.com>
2022-11-24 23:18:20 +0530

    Selenium LoginPage

---
+++ AutomationTest/src/test/java/Selenium/Login.java
@@ -0,0 +1,16 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+	
+	WebDriver driver;
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", ".driver/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demoqa.com/");
+	}
+
+}
